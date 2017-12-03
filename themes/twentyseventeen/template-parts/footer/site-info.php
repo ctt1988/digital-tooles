57e0c3cec19677a9722490a0712c9265a88aa22d
Ubuntu <clarence.tooles@gmail.com>
2017-12-03 03:07:58 +0000

    added git

---
+++ themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Property of %s', 'twentyseventeen' ), 'Digital Tooles, Llc' ); ?></a>
+</div><!-- .site-info -->
