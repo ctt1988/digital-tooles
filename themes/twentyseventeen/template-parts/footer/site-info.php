21da77ba9f191acb3b584a8853b8b3e424ca3dce
ctt1988 <clarence.tooles@gmail.com>
2017-12-02 22:22:26 -0500

    removed footer

--- themes/twentyseventeen/template-parts/footer/site-info.php
+++ themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Property of %s', 'twentyseventeen' ), 'Digital Tooles, Llc' ); ?></a>
+
 </div><!-- .site-info -->
